e98fbf798e6697374fb19ce91de2f74eec91c618
Dongyan0326-coder <dongyanzhang0326@gmail.com>
2026-01-06 01:03:06 +0800

    init : 2026.01.05

---
+++ DS/code/shell_sort.py
@@ -0,0 +1,16 @@
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            # 插入排序
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(shell_sort(arr))
